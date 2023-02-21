f22f383df27564c6b360c4228162b561b3a27e48
ImGyuriKim <gyurikim0105@gmail.com>
2023-02-21 13:44:09 +0900

    [front] feat: 질문하기 페이지 생성 질문하기 버튼 생성 contributors: ImGyuriKim

--- client/src/components/QButton.js
+++ client/src/components/QButton.js
@@ -1,6 +1,21 @@
+import { Button } from '@mui/material';
+import styled from 'styled-components';
+
 //* 질문하기 버튼 -> Ask 페이지로 이동
+const Container = styled.div`
+  button {
+    background-color: #0b95ff;
+    color: white;
+    font-size: 12px;
+    padding: 10px;
+  }
+`;
 function QButton() {
-  return <button>ask question</button>;
+  return (
+    <Container>
+      <Button>Ask Question</Button>
+    </Container>
+  );
 }
 
 export default QButton;
--- client/src/pages/Ask.js
+++ client/src/pages/Ask.js
@@ -1,12 +1,16 @@
 import AskForm from '../components/AskForm';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 //* VIEW_02 질문 새로 입력하기
 function Ask() {
   return (
     <div>
-      <h1>Ask page</h1>
+      <Header />
+      <h1>Ask a public question</h1>
       <AskForm />
       <button>submit</button>
+      <Footer />
     </div>
   );
 }
